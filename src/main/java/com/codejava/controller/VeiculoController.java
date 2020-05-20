a94623d73a95d70dc1dfe10ee4f2d88b60b79ecb
Jardim <gabrielrlj99@outlook.com>
2020-05-19 21:21:23 -0300

    atualização com correção de erros

--- src/main/java/com/codejava/controller/VeiculoController.java
+++ src/main/java/com/codejava/controller/VeiculoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.codejava.model.Empresa;
+import com.codejava.model.TipoVeiculo;
 import com.codejava.model.Veiculo;
 import com.codejava.services.EmpresaService;
 import com.codejava.services.VeiculoService;
@@ -23,51 +24,61 @@ public class VeiculoController {
 	private VeiculoService serviceVeiculo;
 	@Autowired
 	private EmpresaService serviceEmpresa;
-	
+
 	@RequestMapping("/listaVeiculos/{id_empresa}")
-	public String ViewHomePage(Model model, @PathVariable(name="id_empresa") Long id) {
+	public String ViewHomePage(Model model, @PathVariable(name = "id_empresa") Long id) {
 		Empresa empresa = new Empresa();
 		empresa = serviceEmpresa.get(id);
-		
+
 		List<Veiculo> listaVeiculos = serviceVeiculo.ListPorEmpresa(empresa);
 		model.addAttribute("listaVeiculos", listaVeiculos);
 		return "Index_veiculos";
 	}
-	
+
 	@RequestMapping("/novo_veiculo/{id_empresa}")
-	public String formularioNovoVeiculo(Model model ,@PathVariable(name="id_empresa") Long id) {
+	public String formularioNovoVeiculo(Model model, @PathVariable(name = "id_empresa") Long id) {
 		Veiculo veiculo = new Veiculo();
 		Empresa empresa = new Empresa();
 		empresa = serviceEmpresa.get(id);
-		
+
 		model.addAttribute("empresa", empresa);
 		model.addAttribute("veiculo", veiculo);
 		return "novo_veiculo";
 	}
-	
+
 	@GetMapping("/salvar_veiculo/{id_empresa}")
-	public String salvarEstacionamento(@ModelAttribute("veiculo") Veiculo veiculo, @PathVariable(name="id_empresa") Long id) {
+	public String salvarEstacionamento(@ModelAttribute("veiculo") Veiculo veiculo,
+			@PathVariable(name = "id_empresa") Long id) {
 		Empresa empresa = new Empresa();
 		empresa = serviceEmpresa.get(id);
 		veiculo.setEmpresa(empresa);
+
+		if (veiculo.getTipo().equals(TipoVeiculo.CARRO)) {
+			empresa.setQtd_carros((empresa.getQtd_carros() - 1));
+		} else if (veiculo.getTipo().equals(TipoVeiculo.MOTO)) {
+			empresa.setQtd_motos((empresa.getQtd_motos() - 1));
+		}
+
+		serviceEmpresa.saveEmpresa(empresa);
 		serviceVeiculo.saveVeiculo(veiculo);
-		
+
 		return "redirect:/listaVeiculos/{id_empresa}";
-		
+
 	}
-	
-	@RequestMapping("/editarVeiculo/{id_veiculo}")
-	public ModelAndView formularioEditarEstacionamento(@PathVariable(name = "id_veiculo") Long id) {
-		ModelAndView mav = new ModelAndView("editar_veiculo");
-		
-		Veiculo veiculo = serviceVeiculo.get(id);
-		mav.addObject("veiculo", veiculo);
-		
-		return mav;
-	}
-	
+
 	@RequestMapping("/deletarVeiculo/{id_veiculo}")
 	public String deletarEmpresa(@PathVariable(name = "id_veiculo") Long id) {
+		Veiculo v = new Veiculo();
+		v = serviceVeiculo.get(id);
+		Empresa empresa = new Empresa();
+		empresa = serviceEmpresa.get(v.getIdEmpresa());
+		if (v.getTipo().equals(TipoVeiculo.CARRO)) {
+			empresa.setQtd_carros((empresa.getQtd_carros() + 1));
+		} else if (v.getTipo().equals(TipoVeiculo.MOTO)) {
+			empresa.setQtd_motos((empresa.getQtd_motos() + 1));
+		}
+
+		serviceEmpresa.saveEmpresa(empresa);
 		serviceVeiculo.delete(id);
 		return "redirect:/";
 	}
