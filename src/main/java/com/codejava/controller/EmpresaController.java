02f1f32e13e20e24640eaac2aca7e0d415f6424e
Jardim <gabrielrlj99@outlook.com>
2020-05-25 12:45:38 -0300

    retornando ao que estava antes

--- src/main/java/com/codejava/controller/EmpresaController.java
+++ src/main/java/com/codejava/controller/EmpresaController.java
@@ -20,7 +20,7 @@ public class EmpresaController {
 	private EmpresaService service;
 	
 	
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String ViewHomePage(Model model) {
 		List<Empresa> listaEmpresas = service.listAll();
 		model.addAttribute("listaEmpresas", listaEmpresas);
